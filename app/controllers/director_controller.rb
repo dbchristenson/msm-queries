0e57defdf33366b7c46e281b75f8b007e9cedacc
DB Christenson <dbeauchristenson@gmail.com>
2024-05-16 21:22:52 +0000

    function match

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -8,12 +8,22 @@ class DirectorController < ApplicationController
     @director_id = params.fetch("director_id")
     @director = Director.all.where( {:id => @director_id} )[0]
     
-    render( {:template => "/director_templates/selected"})
+    render({ :template => "/director_templates/selected" })
   end
 
   def youngest
+    no_nils = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    earliest_dob = no_nils.maximum(:dob)
+    @director = Director.where({ :dob => earliest_dob })[0]
+
+    render({ :template => "/director_templates/youngest" })
   end
 
   def oldest
+    no_nils = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    latest_dob = no_nils.minimum(:dob)
+    @director = Director.where({ :dob => latest_dob })[0]
+
+    render({ :template => "/director_templates/oldest" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", { :controller => "director", :action => "all" })
-  get("/directors/:director_id", { :controller => "director", :action => "selected" })
   get("/directors/youngest", { :controller => "director", :action => "youngest"})
-  get("/directors/oldest", { :controller => "director", :action => "oldest"})
-
+  get("/directors/eldest", { :controller => "director", :action => "oldest"})
+  get("/directors/:director_id", { :controller => "director", :action => "selected" })
+  
   get("/movies", { :controller => "movie", :action => "all" })
   get("/movies/:movie_id", { :controller => "movie", :action => "selected" })
 
